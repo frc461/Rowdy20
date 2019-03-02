9bb2c7035749796f17b588417e333b323d7e3516
Beck <beck@beckteck.org>
2019-03-02 14:57:06 -0500

    Controls

---
+++ src/main/include/Control.h
@@ -0,0 +1,14 @@
+#ifndef SRC_CONTROL_H_
+#define SRC_CONTROL_H_
+
+#include <frc/WPILib.h>
+
+class Control {
+    public:
+
+    private:
+        Control();
+        
+};
+
+#endif 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,16 +1,10 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #pragma once
 
 #include <string>
 
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
+#include <WPILib.h>
+
+#include <Control.h>
 
 class Robot : public frc::TimedRobot {
  public:
@@ -23,8 +17,9 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+  Controls *controls;
+  Joystick *leftJoystick;
+  Joystick *rightJoystick;
+  Joystick *override;
+  Joystick *operator;
 };
